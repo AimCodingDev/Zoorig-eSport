e98aa6f95ade0473c1a95408edb12f02b99ccabc
AimCodingDev <marco.meichtry@aimcoding.dev>
2019-03-28 22:20:06 +0100

    Player

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,30 @@ import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {AboutUsComponent} from './about-us/about-us.component';
 import {GamesComponent} from './games/games.component';
+import {RocketLeagueComponent} from './games/rocket-league/rocket-league.component';
+import {CallOfDutyComponent} from './games/call-of-duty/call-of-duty.component';
+import {FifaComponent} from './games/fifa/fifa.component';
+import {SuperSmashBrosComponent} from './games/super-smash-bros/super-smash-bros.component';
+import {FortniteComponent} from './games/fortnite/fortnite.component';
+import {GameComponent} from './games/game/game.component';
 
 const routes: Routes = [
-  {path: 'home', component: HomeComponent},
-  {path: 'about_us', component: AboutUsComponent},
-  {path: 'games', component: GamesComponent},
-  {path: '', component: HomeComponent}
+  {path: '', redirectTo: 'home', pathMatch: 'full' },
+  {path: '', component: HomeComponent},
+  {path: 'about-us', component: AboutUsComponent},
+  {path: 'games',
+    component: GamesComponent,
+    children: [
+      {path: '', component: GameComponent},
+      {path: 'rocket-league', component: RocketLeagueComponent},
+      {path: 'call-of-duty', component: CallOfDutyComponent},
+      {path: 'fifa', component: FifaComponent},
+      {path: 'super-smash-bros', component: SuperSmashBrosComponent},
+      {path: 'fortnite', component: FortniteComponent},
+      {path: '**', redirectTo: 'profile', pathMatch: 'full' }
+    ]
+  },
+  {path: '**', redirectTo: 'home', pathMatch: 'full' }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,13 @@ import { HomeComponent } from './home/home.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { FooterComponent } from './footer/footer.component';
 import { GamesComponent } from './games/games.component';
+import { RocketLeagueComponent } from './games/rocket-league/rocket-league.component';
+import { FifaComponent } from './games/fifa/fifa.component';
+import { SuperSmashBrosComponent } from './games/super-smash-bros/super-smash-bros.component';
+import { CallOfDutyComponent } from './games/call-of-duty/call-of-duty.component';
+import { FortniteComponent } from './games/fortnite/fortnite.component';
+import { DropdownDirective } from './shared/dropdown.directive';
+import { GameComponent } from './games/game/game.component';
 
 @NgModule({
   declarations: [
@@ -17,6 +24,13 @@ import { GamesComponent } from './games/games.component';
     AboutUsComponent,
     FooterComponent,
     GamesComponent,
+    RocketLeagueComponent,
+    FifaComponent,
+    SuperSmashBrosComponent,
+    CallOfDutyComponent,
+    FortniteComponent,
+    DropdownDirective,
+    GameComponent,
   ],
   imports: [
     BrowserModule,
