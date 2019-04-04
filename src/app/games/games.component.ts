99eda6f851d3ce4016b8c95b5049ef0e26d9d5c6
AimCodingDev <marco.meichtry@aimcoding.dev>
2019-04-04 20:08:06 +0200

    routing

--- src/app/games/games.component.ts
+++ src/app/games/games.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+/* tslint:disable:no-trailing-whitespace */
+import {AfterContentChecked, Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-games',
   templateUrl: './games.component.html',
   styleUrls: ['./games.component.sass']
 })
-export class GamesComponent implements OnInit {
+export class GamesComponent implements OnInit{
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
   }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterContentChecked, Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.sass']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, AfterContentChecked {
+  public href = '';
+  public x = this.router.isActive('/games', false);
 
-  constructor() { }
+  constructor(private router: Router) {}
+
+  ngAfterContentChecked() {
+    this.href = this.router.url;
+    console.log(this.router.url);
+  }
 
   ngOnInit() {
   }
